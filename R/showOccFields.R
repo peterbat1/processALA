198e8f60d78c43a66bf4524fcbbe944f6515d865
Peter D Wilson <peterdonaldwilson@gmail.com>
2020-10-31 18:25:16 +1100

    fixed a small typo in roxygen markup for function help in showOccFields()

--- R/showOccFields.R
+++ R/showOccFields.R
@@ -5,15 +5,15 @@
 #'
 #' @return A dataframe listing all ALA-supplied information for the specified set of fields.
 #' @export
-#' @details {
-#' The returned dataframe shows all the information associated with the set of fields specified by 'fieldSet'. This is very large body of information. The values which must be supplied to \link{fetchALAdata} in the parameter 'fieldSet' are found in the 'name' column.
+#' @details
+#' The returned dataframe shows all the information associated with the set of fields specified by \emph{fieldSet}. This is very large body of information. The values which must be supplied to \link{fetchALAdata} in the parameter \emph{fieldSet} are found in the 'name' column.
 #'
-#' The default value for 'fieldSet' is 'standard' and this returns the set of fields used for a default download of records; 'all' shows all available ALA occurrence fields which may be used for customised calls to the function \link{fetchALAdata}.
+#' The default value for \emph{fieldSet} is 'standard' and this returns the set of fields used for a default download of records; 'all' shows all available ALA occurrence fields which may be used for customised calls to the function \link{fetchALAdata}.
 #'
 #' Note that this function is simple wrapper around the ALA4R function \link{ala_fields}.
-#' }
+#'
 #' @examples
-#' \dontrun {
+#' \dontrun{
 #' ## Return the standard occurrence fields used in processALA:
 #' stdStuff <- showOccFields()
 #'
